5b1037145f873d46af768aa3f559b49f39797127
Tam Dang <danhtam0509@gmail.com>
2019-03-08 00:43:15 -0500

    Modified

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      socket: socketIOClient('https://taha-chatapp.herokuapp.com/'),
+      socket: socketIOClient(),
     }
   }
 
